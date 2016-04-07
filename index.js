c854ace27dfab73cadbeec4e212e40ff74298960
Edward Smith <edwardmsmith@gmail.com>
2016-04-07 11:27:40 -0500

    Initial commit...

---
+++ index.js
@@ -0,0 +1,58 @@
+"use strict"
+const CamayakContentAPI = require('camayak-contentapi');
+const CamayakMedium     = require('./lib/camayak_medium');
+
+const api_key       = process.env.CAMAYAK_API_KEY;
+const shared_secret = process.env.CAMAYAK_SHARED_SECRET;
+const medium_key    = process.env.MEDIUM_API_KEY;
+
+// Proposed usage of a Camayak-contentapi sdk
+let camayak = new CamayakContentAPI({
+    api_key: api_key,
+    shared_secret: shared_secret,
+    publish: function(webhook, content) {
+        let handler = new CamayakMedium(medium_key);
+        handler.publish(content, function(error, response){
+            if (error) {
+                return webhook.fail(error);
+            };
+            return webhook.succeed({
+                published_id: response.published_id,
+                published_url: response.published_url
+            });
+        });
+    },
+    update: function(webhook, content) {
+        let handler = new CamayakMedium(medium_key);
+        handler.update(content, function(error, response){
+            if (error) {
+                return webhook.fail(error);
+            };
+            return webhook.succeed({
+                published_id: response.published_id,
+                published_url: response.published_url
+            });
+        });
+    },
+    retract: function(webhook, content) {
+        // Retract Post using content.published_id
+        //
+        let handler = new CamayakMedium(medium_key);
+        handler.retract(content, function(error, response){
+            if (error) {
+                return webhook.fail(error);
+            };
+            return webhook.succeed({
+                published_id: response.published_id,
+                published_url: response.published_url
+            });
+        })
+    },
+    error: function(error, webhook) {
+        // Handle unexpected errors in the Camayak service
+        webhook.fail(error);
+    }
+});
+
+// Start listening for webhooks
+camayak.start();
